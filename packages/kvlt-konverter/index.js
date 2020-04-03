6d245daef8eeb5d632cdbdfdb233d1f56247ebb9
jacobtheevans <jakegod14@gmail.com>
2020-04-03 19:14:33 +0200

    Separate code to control class

--- packages/kvlt-konverter/index.js
+++ packages/kvlt-konverter/index.js
@@ -1,35 +1,5 @@
-function convertGodToSatan (str) {
-  return str.replace(/god/ig, 'satan')
-}
-
-function convertUToV (str) {
-  const lowerCase = str.replace(/u/g, 'v')
-  const upperCase = lowerCase.replace(/U/g, 'V')
-  return upperCase
-}
-
-function convertCToK (str) {
-  const lowerCase = str.replace(/c/g, 'c')
-  const upperCase = lowerCase.replace(/C/g, 'C')
-  return upperCase
-}
-
-function convertOToÖ (str) {
-  const lowerCase = str.replace(/o/g, 'ö')
-  const upperCase = lowerCase.replace(/O/g, 'Ö')
-  return upperCase
-}
-
-function convertGenresToTrveNorwegianBlackMetal (str, genres = []) {
-  let result = str
-  for (const genre of genres) {
-    result = result.replace(
-      RegExp(genre, 'ig'),
-      'trve nörwegian black metal'
-    )
-  }
-  return result
-}
+const KvltKonverter = require('./KvltKonverter')
+const kvltKonverter = new KvltKonverter()
 
 function convertStringToKvlt (
   str,
@@ -44,28 +14,26 @@ function convertStringToKvlt (
 ) {
   let result = str
   if (convertGodToSatan) {
-    result = convertGodToSatan(result)
+    result = kvltKonverter.convertGodToSatan(result)
   }
   if (convertGenresToTrveNorwegianBlackMetal) {
-    result = convertGenresToTrveNorwegianBlackMetal(result, genres)
+    result = kvltKonverter.convertGenresToTrveNorwegianBlackMetal(
+      result,
+      genres
+    )
   }
   if (convertUToV) {
-    result = convertUToV(result)
+    result = kvltKonverter.convertUToV(result)
   }
   if (convertCToK) {
-    result = convertCToK(result)
+    result = kvltKonverter.convertCToK(result)
   }
   if (convertOToÖ) {
-    result = convertOToÖ(result)
+    result = kvltKonverter.convertOToÖ(result)
   }
   return result
 }
 
 module.exports = {
-  convertGodToSatan,
-  convertUToV,
-  convertCToK,
-  convertOToÖ,
-  convertGenresToTrveNorwegianBlackMetal,
   convertStringToKvlt
 }
