c9a745e952e0e6f54b11c179a83269944ab18300
jacobtheevans <jakegod14@gmail.com>
2020-05-23 16:23:57 +0200

    Testing of ci pipeline that builds images and package on every commit

---
+++ packages/kvlt-konverter/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd $(dirname `[[ $0 = /* ]] && echo "$0" || echo "$PWD/${0#./}"`)
+
+NPM_EMAIL=$1
+NPM_TOKEN=$2
+
+rm .npmrc
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
+
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+NPM_PACAKGE_VERSION=$(npm view kvlt-konverter version)
+
+if [ "$PACKAGE_VERSION" != "$NPM_PACAKGE_VERSION" ]; then
+  echo "Package has been updated publishing..."
+  npm publish
+fi
---
+++ packages/kvlt-telegram-bot/release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd $(dirname `[[ $0 = /* ]] && echo "$0" || echo "$PWD/${0#./}"`)
+
+DOCKER_USERNAME=$1
+DOCKER_PASSWORD=$2
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+GIT_COMMIT=$(git rev-parse HEAD)
+
+docker build -t jacobtheevans/kvlt-telegram-bot:${GIT_COMMIT} .
+
+docker push jacobtheevans/kvlt-telegram-bot:${GIT_COMMIT}
