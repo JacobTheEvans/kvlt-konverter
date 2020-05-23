6942926b2d72dc5f8f066e8eaa1b3caf7986bed0
jacobtheevans <jakegod14@gmail.com>
2020-05-23 17:04:46 +0200

    Add debugging message to kvlt-konverter release.sh script

--- packages/kvlt-konverter/release.sh
+++ packages/kvlt-konverter/release.sh
@@ -11,6 +11,8 @@ echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 NPM_PACAKGE_VERSION=$(npm view kvlt-konverter version)
 
+echo "Current version on npm ${NPM_PACAKGE_VERSION}"
+echo "Current version in package.json ${PACKAGE_VERSION}"
 if [ "$PACKAGE_VERSION" != "$NPM_PACAKGE_VERSION" ]; then
   echo "Package has been updated publishing..."
   npm publish
