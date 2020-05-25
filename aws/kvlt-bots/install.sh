da9c3c30d4693708939477ebf98c934d9e356243
jacobtheevans <jakegod14@gmail.com>
2020-05-25 17:24:05 +0200

    make sure dir is there before installing in the aws scripts

--- aws/kvlt-bots/install.sh
+++ aws/kvlt-bots/install.sh
@@ -11,6 +11,7 @@ unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 rm ~/.aws/config
+mkdir -p ~/.aws
 echo "[default]" >  ~/.aws/config
 echo "aws_access_key_id=${AWS_ACCESS_KEY_ID}" >>>  ~/.aws/config
 echo "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" >>>  ~/.aws/config
