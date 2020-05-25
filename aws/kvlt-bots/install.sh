b0bafd41654954b1107835092348708dbb463382
jacobtheevans <jakegod14@gmail.com>
2020-05-25 17:33:50 +0200

    remove unneeded >

--- aws/kvlt-bots/install.sh
+++ aws/kvlt-bots/install.sh
@@ -13,8 +13,8 @@ sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 rm ~/.aws/config
 mkdir -p ~/.aws
 echo "[default]" >  ~/.aws/config
-echo "aws_access_key_id=${AWS_ACCESS_KEY_ID}" >>>  ~/.aws/config
-echo "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" >>>  ~/.aws/config
-echo "region=${REGION}" >>>  ~/.aws/config
+echo "aws_access_key_id=${AWS_ACCESS_KEY_ID}" >>  ~/.aws/config
+echo "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" >>  ~/.aws/config
+echo "region=${REGION}" >>  ~/.aws/config
 
 npm install
