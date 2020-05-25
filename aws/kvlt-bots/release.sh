b61f56f491b70f95f85b37f89710d0d468a89c5b
jacobtheevans <jakegod14@gmail.com>
2020-05-25 17:41:05 +0200

    fix release being passed to cdk deploy in the release script

--- aws/kvlt-bots/release.sh
+++ aws/kvlt-bots/release.sh
@@ -3,4 +3,4 @@
 cd $(dirname `[[ $0 = /* ]] && echo "$0" || echo "$PWD/${0#./}"`)
 
 GIT_COMMIT=$(git rev-parse HEAD)
-${GIT_COMMIT} ./node_modules/.bin/cdk deploy -ci
+RELEASE=$GIT_COMMIT ./node_modules/.bin/cdk deploy -ci
