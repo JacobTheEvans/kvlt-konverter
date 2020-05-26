00206b44968fa85671e1a8a8c611da4499b9b0e9
jacobtheevans <jakegod14@gmail.com>
2020-05-26 15:06:10 +0200

    Add region to aws config file

--- aws/kvlt-bots/install.sh
+++ aws/kvlt-bots/install.sh
@@ -15,6 +15,9 @@ mkdir -p ~/.aws
 echo "[default]" >  ~/.aws/credentials
 echo "aws_access_key_id=${AWS_ACCESS_KEY_ID}" >>  ~/.aws/credentials
 echo "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" >>  ~/.aws/credentials
-echo "region=${REGION}" >>  ~/.aws/credentials
+
+echo "[default]" >  ~/.aws/config
+echo "region=${REGION}" >>  ~/.aws/config
+echo "output=json" >>  ~/.aws/config
 
 npm install
