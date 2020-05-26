45936a78988a71b54ae000474cd109d822bcd1ca
jacobtheevans <jakegod14@gmail.com>
2020-05-26 14:32:54 +0200

    Bypass accepting in console in release.sh of aws

--- aws/kvlt-bots/release.sh
+++ aws/kvlt-bots/release.sh
@@ -3,4 +3,4 @@
 cd $(dirname `[[ $0 = /* ]] && echo "$0" || echo "$PWD/${0#./}"`)
 
 GIT_COMMIT=$(git rev-parse HEAD)
-RELEASE=$GIT_COMMIT ./node_modules/.bin/cdk deploy -ci
+RELEASE=$GIT_COMMIT ./node_modules/.bin/cdk deploy --require-approval=never
