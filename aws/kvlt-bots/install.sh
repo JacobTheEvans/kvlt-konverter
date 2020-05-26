281b37b8e7168bd96bfefe5fad24eded170430bf
jacobtheevans <jakegod14@gmail.com>
2020-05-26 15:28:22 +0200

    Add debug output to install scripts

--- aws/kvlt-bots/install.sh
+++ aws/kvlt-bots/install.sh
@@ -10,14 +10,15 @@ curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
-rm ~/.aws/config
 mkdir -p ~/.aws
 echo "[default]" >  ~/.aws/credentials
 echo "aws_access_key_id=${AWS_ACCESS_KEY_ID}" >>  ~/.aws/credentials
 echo "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" >>  ~/.aws/credentials
+cat ~/.aws/credentials
 
 echo "[default]" >  ~/.aws/config
 echo "region=${REGION}" >>  ~/.aws/config
 echo "output=json" >>  ~/.aws/config
+cat ~/.aws/config
 
 npm install
--- aws/kvlt-bots/release.sh
+++ aws/kvlt-bots/release.sh
@@ -3,4 +3,4 @@
 cd $(dirname `[[ $0 = /* ]] && echo "$0" || echo "$PWD/${0#./}"`)
 
 GIT_COMMIT=$(git rev-parse HEAD)
-RELEASE=$GIT_COMMIT ./node_modules/.bin/cdk deploy --require-approval=never
+RELEASE=$GIT_COMMIT ./node_modules/.bin/cdk deploy -ci --require-approval=never --profile personal
